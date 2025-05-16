6c32628cbd882f7b39b30b47c4f2374884c51552
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-05-15 18:54:25 -0600

    Comentarios ampliados

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,31 +1,36 @@
-#Definir las secciones
+#Lista que se encarga de definir las secciones de la universidad
 secciones = ["c-101", "b-202", "m-303"]
 
-#Total de asistencias acumuladas
+#Variable que permite guardar el total de asistencias acumuladas
 asistencias_totales = 0
 
-#Bucle externo (Secciones)
+#Bucle externo encargado de recorrer cada sección en la lista
 for seccion in secciones:
     print(f"n\+++{seccion}+++")
     asistencias_seccion = 0
     
-    #bucle de días
+    #Bucle encargado de recorrer los días de la semana
     for dia in range (1,6):
         print(f"\n Día: {dia}")
         
-        #bucle de los 6 estudiantes seleccionados.
+        #bucle encargado de recorrer los 6 estudiantes de la lista.
         for estudiante in range(1,7):
+            
+            #Validación de entrada: Para marcar la asistencia de los estudiantes.
             while True:
                 asistencia = int(input(f"¿Asistio a clases el estudiante {estudiante}? 1 = Sí 0 = No "))
                 if asistencia in (1,0):
                     break
                 else:
                     print("¡¡ERR000R!! Ingrese 1 o 0")
-                
+            
+            #Sumatoria que va acumulando las asistencias en la variable de asistencias por sección
             asistencias_seccion += asistencia
             
     #Mostrar todas las asistencias por sección
     print(f"\n Total de asistencias en la sección {seccion}: {asistencias_seccion}")
+    
+    #Sumatoria de todas las asistencias por sección para así acumularlas en la variable general
     asistencias_totales += asistencias_seccion
 
 #Mostrar las asistencias totales (Sumatoria total de las asistencias en todas las secciones)
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,41 +1,61 @@
-import random
+import random #Importar la librería random para así generar números aleatorios
 
-#Definir los dos laboratorios
+#Lista de laboratorios
 laboratorios = ["Laboratorio A","laboratorio B" ]
 
-#Bucle para conocer los espacios ocpuados y disponibles.
+#Bucle externo: Encargado de recorrer los dos laboratorios en la lista
 for lab in laboratorios:
     print(f"\n---{lab}---")
+    
+    #Variables para almacenar la información correspondiente
     ocupados = 0
     disponibles = 0
     
-    #Preguntar si se desea ingresar datos manualmente o de forma aleatoria
-    modo = input("Seleccione el modo de analisis: i = Ingresar, r = Randomizer ")    
+    #Solicitud de si se desea ingresar datos manualmente o de forma aleatoria
+    modo = input("Seleccione el modo de analisis: i = Ingresar, r = Randomizer ")
     
-    #Numero de filas y computadoras usadas
+    #Validación de selección de modo manual
+    while modo not in ('i', 'r'):
+        modo = input("Modo inválido. Seleccione: i = Ingresar, r = Randomizer ")
+    
+    #Bucle medio: Encargado de recorrer el número de filas a regirstrar
     for fila in range(1,6):
         print(f"\nFila: {fila}")
+        
+        #Bucle interno encargado de recorrer las 4 computadoras por fila
         for computadora in range (1,5):
+            
+            #Comando por si se seleccionó el modo manual
             if modo == 'i':
-                #Modo manual
+                
+                #Validación de estado de computadoras (Si se ingresa de forma manual los datos)
                 while True:
                     estado = int(input(f"¿La computadora está ocupada? 1 = Si, 0 = No "))
                     if estado in (0,1):
                         break
-                    else:
-                        print("Error... Selección no valida")
+                    
+                    #En caso de no ingresar los valores asignados se ejecutara este comando:
+                    while estado not in (0,1):
+                        print(f"\n¡¡ERR0R!! intentelo de nuevo")
+                        break
             else:
                 #Modo simulado (Random)
                 estado = random.randint(0,1)
                 print(f"Computadora {computadora}: {'Ocupada' if estado == 1 else 'Libre'}")
         
         #Acumulación de datos del bucle
+        #Si el estado es igual a 1.
+        #Se almacenara en la variable "Ocupados"
         if estado == 1:
             ocupados += 1
+            
+        #Si el estado es igual a 0
+        #La información se almacenará en la variable "Disponibles"
         else:
             disponibles += 1
     
-    #Devolver datos (Libres y ocupados)
+    #Devolver datos 
+    # Resumenes de ambos laboratorios y que espacios están libres y ocupados
     print(f"\nResumen en {lab}:")
     print(f"Computadoras ocupadas: {ocupados}")
     print(f"Computadoras libres: {disponibles}")
